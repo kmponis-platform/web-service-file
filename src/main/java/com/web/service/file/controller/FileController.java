4be20ce0d230ca3e97c9143ecb718178b437c818
kbonis <kbonis@ITEM-S54177.mshome.net>
2019-04-04 21:22:56 +0100

    Add post and get url for testing

--- src/main/java/com/web/service/file/controller/FileController.java
+++ src/main/java/com/web/service/file/controller/FileController.java
@@ -1,8 +1,10 @@
 package com.web.service.file.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import com.web.service.file.service.FileService;
 import io.swagger.annotations.ApiOperation;
@@ -17,6 +19,24 @@ public class FileController {
   @PostMapping(value = "/open")
   public void open(@RequestBody String filePath) {
     fileService.openDocument(filePath);
+  }
+
+  @ApiOperation(value = "Do a post request: ", response = String.class)
+  @PostMapping(value = "/post")
+  public String post(@RequestParam(value = "a") String a) {
+    if ("madrid".equalsIgnoreCase(a)) {
+      return "This is a post: Hola " + a + " we are comming!";
+    }
+    return "This is a post: Parameter " + a;
+  }
+
+  @ApiOperation(value = "Do a get request: ", response = String.class)
+  @GetMapping(value = "/get")
+  public String get(@RequestParam(value = "a") String a) {
+    if ("madrid".equalsIgnoreCase(a)) {
+      return "This is a get: Hola " + a + " we are comming!";
+    }
+    return "This is a get: Parameter " + a;
   }
 
 }
